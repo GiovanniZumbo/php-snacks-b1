ba79d14eaecdc2746b2c050714297555a48d6c30
Giovanni Zumbo <gianni.zu07@gmail.com>
2024-10-03 17:20:37 +0200

    completed

--- index.php
+++ index.php
@@ -1,3 +1,65 @@
+<?php
+
+$posts = [
+
+    '10/01/2019' => [
+        [
+            'title' => 'Post 1',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 1'
+        ],
+        [
+            'title' => 'Post 2',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 2'
+        ],
+    ],
+    '10/02/2019' => [
+        [
+            'title' => 'Post 3',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 3'
+        ]
+    ],
+    '15/05/2019' => [
+        [
+            'title' => 'Post 4',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 4'
+        ],
+        [
+            'title' => 'Post 5',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 5'
+        ],
+        [
+            'title' => 'Post 6',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 6'
+        ]
+    ],
+
+    '23/05/2019' => [
+        [
+            'title' => 'Post 7',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 7'
+        ],
+        [
+            'title' => 'Post 8',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 8'
+        ],
+        [
+            'title' => 'Post 9',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 9'
+        ]
+    ],
+];
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -9,7 +71,21 @@
 
 <body>
     <div class="container">
-        <div></div>
+        <div>
+            <ul>
+                <?php foreach ($posts as $day => $postInDay) { ?>
+                    <h3> Giorno: <?= $day ?> </h3>
+                    <ul>
+                        <?php foreach ($postInDay as $post) { ?>
+                            <li> Titolo: <?= $post['title']; ?> </li>
+                            <li> Autore: <?= $post['author']; ?></li>
+                            <li> Testo: <?= $post['text']; ?></li>
+                            <hr>
+                        <?php } ?>
+                    </ul>
+                <?php } ?>
+            </ul>
+        </div>
     </div>
 
 </body>
