0d4517d1b56e1d4cfb236a86df2f0d2f436c5d4d
Giovanni Zumbo <gianni.zu07@gmail.com>
2024-10-03 12:48:29 +0200

    first commit

---
+++ data.php
@@ -0,0 +1,59 @@
+<?php
+
+$posts = [
+
+    '10/01/2019' => [
+        [
+            'title' => 'Post 1',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 1'
+        ],
+        [
+            'title' => 'Post 2',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 2'
+        ],
+    ],
+    '10/02/2019' => [
+        [
+            'title' => 'Post 3',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 3'
+        ]
+    ],
+    '15/05/2019' => [
+        [
+            'title' => 'Post 4',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 4'
+        ],
+        [
+            'title' => 'Post 5',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 5'
+        ],
+        [
+            'title' => 'Post 6',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 6'
+        ]
+    ],
+
+    '23/05/2019' => [
+        [
+            'title' => 'Post 7',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 7'
+        ],
+        [
+            'title' => 'Post 8',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 8'
+        ],
+        [
+            'title' => 'Post 9',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 9'
+        ]
+    ],
+];
