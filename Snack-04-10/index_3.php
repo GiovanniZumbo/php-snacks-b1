6ea0e62a6ea5014b84e5b38f00c1becd568d11b7
Giovanni Zumbo <gianni.zu07@gmail.com>
2024-10-04 13:16:16 +0200

    completed

--- Snack-04-10/index_3.php
+++ Snack-04-10/index_3.php
@@ -14,26 +14,27 @@ include "./staff.php";
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css' integrity='sha512-jnSuA4Ss2PkkikSOLtYs8BlYIeeIK1h99ty4YfvRPAlzr377vr3CXDb7sb7eEEBYjDtcYj+AjBH3FLv5uSJuXg==' crossorigin='anonymous' />
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
-    <h2>Insegnanti</h2>
 
-    <div class="border border-dark">
-        <?php foreach ($db as $teachers => $teacherInDb) { ?>
-            <ul>
-                <?php foreach ($teacherInDb as $teacher) { ?>
-                    <li> Nome: <?= $teacher["name"] ?></li>
-                    <li> Cognome: <?= $teacher["lastname"] ?></li>
-                <?php } ?>
-                <li></li>
-            </ul>
+    <div class="container">
+        <?php foreach ($db as $people => $peopleInDb) { ?>
+            <div class="staff">
+                <h2><?= $people ?></h2>
+
+                <div>
+                    <?php foreach ($peopleInDb as $teacher) { ?>
+                        <ul>
+                            <li> Nome: <?= $teacher["name"] ?></li>
+                            <li> Cognome: <?= $teacher["lastname"] ?></li>
+                        </ul>
+                    <?php } ?>
+                </div>
+            </div>
         <?php  } ?>
-
-
     </div>
-
-    <h2>PM</h2>
 
 </body>
 
